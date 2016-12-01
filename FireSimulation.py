64af4dd01d6219cc6543a178e8f068dc077abf7a
WillClaudeHolmes <will.claude.holmes@gmail.com>
2016-12-01 15:04:12 -0600

    Fire Simulation First Working Version

--- FireSimulation.py
+++ FireSimulation.py
@@ -7,24 +7,42 @@ from matplotlib import pyplot as plt
 class FireGrid(CAGrid):
     """This class derives from CAGrid and changes update to make this a first simulation"""
 
+
     def Update(self):
         """Update rules for a simple fire simulation"""
 
 
-        print('in set value')
-        for y in range(self.shape[0]):
-            for x in range(self.shape[1]):
-                if self['Tree'][y][x]:
-                    if self['Burning'][y][x]:
-                        self['Tree'][y][x] = False
+        #determine the number of neighbors on fire
+        numpy.place(self.count,self.TrueArray,0)
+        for n in self.Neighbors:
+            self.count = self.count + n['Burning']
 
-                elif self['Tree'][y][x]:
-                    self['Value'][y][x] = (0, 0, 1)
-                else:
-                    self['Value'][y][x] = (1, 1, 1)
+        #update to new values
+        numpy.place(self['Tree'], self['Burning'], False)   # If Burning, new['Tree'] = False else keep old value
+        self['Burning'] = numpy.zeros(self.shape)              # If Burning, new['Burning'] = False (it burned out)
+        #if tree surrounded by fire then set burning, (If tree and count(neighbors burning) * ran > probImmune)
+        numpy.place(self['Burning'], numpy.logical_and(
+            self['Tree'],numpy.multiply(self.count,self.ran) > self['ProbImmune']), True)
+
+        self.FinishUpdate()
+        self.UpdateCount += 1
+
+
+
+
+        #The for loop structure below is not complete.  I think it is OK down to seting trees on fire.
+        # for y in range(self.shape[0]):
+        #    for x in range(self.shape[1]):
+        #        if self['Tree'][y][x]:
+        #            if self['Burning'][y][x]:
+        #                self['Tree'][y][x] = False
+        #
+        #        elif self['Tree'][y][x]:
+        #            self['Value'][y][x] = (0, 0, 1)
+        #        else:
+        #            self['Value'][y][x] = (1, 1, 1)
 
     def SetValue(self):
-        print('in set value')
         for y in range(self.shape[0]):
             for x in range(self.shape[1]):
                 if self['Burning'][y][x]:
--- testdev.py
+++ testdev.py
@@ -1,14 +1,17 @@
+from FireSimulation import *
+
 Rows=3
 Columns=3
 MyDtype = numpy.dtype([('Tree', bool), ('Burning', bool), ('ProbImmune', 'f'), ('Value', 'f', (3,))])
+FireGrid.IncludeDiagonalNeighbors = False
 MyGrid = FireGrid((Rows, Columns), MyDtype)
 #initilize Grid
 for y in range(Rows):
     for x in range(Columns):
-        MyGrid['ProbImmune'][y][x]  = random()
+        MyGrid['ProbImmune'][y][x]  = random() * 4
         if random() < 0.75:
             MyGrid['Tree'][y][x]    = True
-            MyGrid['Burning'][y][x] = random() < 0.1
+            MyGrid['Burning'][y][x] = random() < 0.25
         else:
             MyGrid['Tree'][y][x]    = False
             MyGrid['Burning'][y][x] = False
@@ -16,7 +19,11 @@ for y in range(Rows):
 MyGrid.SetValue()
 MyGrid.SetBoundary()
 print(MyGrid['Value'])
-print(MyGrid['Value'])
 MyFigure, MyAxes = plt.subplots(1,1)
 MyAxes.imshow(MyGrid['Value'])
 MyFigure
+print('starting update')
+MyGrid.Update()
+print('after update')
+print(MyGrid['Value'])
+
